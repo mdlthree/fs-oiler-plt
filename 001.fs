ef0105cdf0aedb5bd484ce7a477ce373a79a5aa4
Matthew <mdlthree@gmail.com>
2015-07-09 09:59:11 -0600

    second solution created

--- 001.fs
+++ 001.fs
@@ -1,3 +1,5 @@
+// first solution
+
 let seqAdd x = x * (x + 1) / 2
 let iPart x y = (x - x % y)/y
 
@@ -7,3 +9,10 @@ let b = 5
 let c = a * b
 
 ((seqAdd (iPart M a))*a) + ((seqAdd (iPart M b))*b) - ((seqAdd (iPart M c))*c)
+
+// second solution
+
+let arithmeticSum multiples maxInt = 
+    List.sum([multiples .. multiples .. maxInt])
+
+printfn "%i" (arithmeticSum 3 1000 + arithmeticSum 5 1000 - arithmeticSum 15 1000)
